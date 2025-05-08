a13b9fa1ea1eea08c98702d279779e21ede1b146
RakeshPegu <rakeshpegu903@gmail.com>
2025-05-08 08:28:56 +0530

    changes in .env file

--- client/react-app/src/lib/apiRequst.ts
+++ client/react-app/src/lib/apiRequst.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const apiRequest = axios.create({
-    baseURL:import.meta.env.BASE_URL,
+    baseURL:import.meta.env.VITE_SERVER_URL,
     withCredentials:true
 
 })
